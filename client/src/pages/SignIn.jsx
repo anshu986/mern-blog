630112d3a71c7de91611ee2d690404054d9fb486
Anshu <ranianshuu986@gmail.com>
2024-02-17 09:56:11 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,26 +1,25 @@
 import { Link, useNavigate } from "react-router-dom"
 import { Alert, Button, Label, TextInput,Spinner } from "flowbite-react";
 import { useState } from "react";
+import {useDispatch,useSelector} from 'react-redux';
+import { signInStart,signInSuccess,signInFailure } from "../redux/user/userSlice";
 
 export default function SignIn ()  {
   const[formData,setFormData]=useState({});
-  const[errorMessage,setErrorMessage]=useState(null);
-  const[loading ,setLoading]=useState(false);
+  const {loading,error:errorMessage}=useSelector((state)=>state.user)
+  const dispatch=useDispatch();
   const navigate=useNavigate();
   const handleChange=(e)=>{
-    setFormData({...formData,[e.target.id]:e.target.value.trim()})
-   
+    setFormData({...formData,[e.target.id]:e.target.value.trim()})   
   }
-  // console.log(formData)
   const handleSubmit=async(e)=>{
     e.preventDefault();
     if(!formData.email||!formData.password)
     {
-      return setErrorMessage("Please fill out all fields")
+      return dispatch(signInFailure("Please fill out all fields"))
     }
     try{
-      setLoading(true);      
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res=await fetch('/api/auth/signin',{
         method:'POST',
         headers:{'Content-Type':'application/json'},
@@ -28,18 +27,17 @@ export default function SignIn ()  {
       });
       const data =await res.json();
       if(data.success==false){
-        return setErrorMessage(data.message)
+        dispatch(signInFailure(data.message))
       }
-      setLoading(false);
       if(res.ok)
       {
+        dispatch(signInSuccess(data));
         navigate('/')
       }
     }
     catch(error)
     {
-      setErrorMessage(error.message)
-      setLoading(false);      
+      dispatch(signInFailure(error.message));    
     }
 
   }
@@ -84,7 +82,7 @@ export default function SignIn ()  {
                   <>
                   <Spinner size='sm'/>
                   <span className='pl-3'>Loading</span></>
-                ):'Sign Inf'
+                ):'Sign In'
               }
             </Button>
           </form>
